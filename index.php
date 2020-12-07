dcb9edb32fb21f480ec16636ca938483d1f9c4b4
Emersonssti <emerson.ssti@gmail.com>
2020-12-06 22:36:55 -0300

    att

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php include "header.php"; ?>
+
+<body class="site-layout--landing preloader-is--active bg-image bg-fixed bg--texture-05">
+
+	<div class="site-wrapper">
+
+		<!-- Header
+		================================================== -->
+		<header id="header" class="site-header site-header--landing">
+		
+		
+			<!-- Logo - Image Based -->
+			<div class="header-logo header-logo--img">
+				<a href="index.html"><img src="assets/img/logo.png" srcset="assets/img/logo@2x.png 2x" alt="Necromancers"></a>
+			</div>
+			<!-- Logo - Image Based / End -->
+		
+	
+		</header>
+		<!-- Header / End -->
+
+		<!-- Content
+		================================================== -->
+		<main class="site-content text-center" id="wrapper">
+			
+			<div class="site-content__center">
+				<h1 class="text-white landing-title"><span class="subtitle landing-subtitle">O maior X1 de Clash of Clans</span>Heroes Tournaments</h1>
+				<a href="home.html" class="btn btn-primary btn-lg btn--landing"><span>Go X1!</span></a>
+			</div>
+			
+		</main>
+
+		<!-- Footer
+		================================================== -->
+		<footer id="footer" class="footer text-center">
+			<ul class="social-menu social-menu--landing social-menu--landing-glitch">
+				<li>
+					<a href="https://www.facebook.com/TorneioHeroes/" target="_blank">
+						<i class="fab fa-facebook-square"></i>
+						<span class="link-subtitle">Facebook</span>Heroes Tournaments
+					</a>
+				</li>
+				<li>
+					<a href="https://twitter.com/torneioheroes" target="_blank">
+						<i class="fab fa-twitter"></i>
+						<span class="link-subtitle">Twitter</span>@torneioheroes
+					</a>
+				</li>
+				<li>
+					<a href="https://discord.com/invite/YvtC9Ns" target="_blank">
+						<i class="fab fa-discord"></i>
+						<span class="link-subtitle">Discord</span>Heroes Tournaments
+					</a>
+				</li>
+			</ul>
+		</footer>
+		<!-- Footer / End -->
+
+	<div class="preloader-overlay">
+		<div id="js-preloader" class="preloader">
+			<div class="preloader-inner fadeInUp">
+				<div class="pong-loader"></div>
+				<svg role="img" class="df-icon df-icon--preloader-arcade">
+					<use xlink:href="assets/img/necromancers.svg#preloader-arcade"/>
+				</svg>
+			</div>
+		</div>
+	</div>
+
+<?php include "footer.php"; ?>
